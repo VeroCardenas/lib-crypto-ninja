4d0f51f49a09b3b3f2ffb1a3a66445f543dc9b00
Veronica Cardenas <vycarden@pichincha.com>
2023-02-17 11:03:10 -0500

    GCM encrypt, decrypt

--- projects/lib-crypto-ninja/src/lib/core/services/encrypt.service.ts
+++ projects/lib-crypto-ninja/src/lib/core/services/encrypt.service.ts
@@ -1,4 +1,5 @@
 import { Inject, Injectable } from '@angular/core';
+import { cipher, util, pkcs5 } from "node-forge";
 
 @Injectable({
   providedIn: 'root',
@@ -17,11 +18,58 @@ export class EncryptService {
 
 
   encryptData(input: any) {
-    console.info(input, this.secretKey, this.secretSalt);
+    if (input === null || input === undefined) {
+      return input;
+    }
+    const dataStr = String(input)
+    const iv = this.getRandomString();
+
+    try {
+      const keyForge = pkcs5.pbkdf2(this.secretKey, this.secretSalt, 256, 32, 'sha256');
+      const aesGcm = cipher.createCipher("AES-GCM", keyForge);
+      aesGcm.start({
+        iv: iv,
+        tagLength: 128
+      });
+      aesGcm.update(util.createBuffer(dataStr, 'utf8'));
+      aesGcm.finish();
+      const data = aesGcm.output.data;
+      const tag = aesGcm.mode.tag.data
+      const encodedB64 = util.encode64(data + tag);
+      return iv + this.encryptionSeparator + encodedB64;
+    } catch (e) {
+      console.error(e);
+      return input;
+    }
   }
 
-  ecryptData(input: any) {
-    console.error(input, this.secretKey, this.secretSalt);
+  decryptData(input: any) {
+    if (input === null || input === undefined || input === '') {
+      return input;
+    }
+    input = String(input)
+    const data = input.split(this.encryptionSeparator)[1]
+    const iv = input.split(this.encryptionSeparator)[0]
+
+    try {
+      const inputDecoded = util.decode64(data)
+      const param = inputDecoded.substring(0, inputDecoded.length - 16)
+      const tag = inputDecoded.substring(inputDecoded.length - 16)
+
+      const keyForge = pkcs5.pbkdf2(this.secretKey, this.secretSalt, 256, 32, 'sha256');
+      const aesGcm = cipher.createCipher("AES-GCM", keyForge);
+      aesGcm.start({
+        iv: iv,
+        tag: util.createBuffer(tag)
+      });
+      aesGcm.update(util.createBuffer(param));
+      aesGcm.finish();
+      return aesGcm.output.data;
+
+    } catch (e) {
+      console.error(e);
+      return null;
+    }
   }
 
   getRandomString() {
--- projects/lib-crypto-ninja/src/public-api.ts
+++ projects/lib-crypto-ninja/src/public-api.ts
@@ -5,3 +5,4 @@
 export * from './lib/lib-crypto-ninja.service';
 export * from './lib/lib-crypto-ninja.component';
 export * from './lib/lib-crypto-ninja.module';
+export * from './lib/core/services'
